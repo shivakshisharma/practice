4dc478bffa4912a63e8d7ce0195887b95f9b7a2b
SHIVAKSHI SHARMA <shivakshisharma2000@gmail.com>
2024-05-03 12:18:33 +0530

    Event listener

--- index.js
+++ index.js
@@ -6,3 +6,50 @@ function b(){
 console.log(window.a);
 console.log(a);
 console.log(this.a);
+
+//Event listeners,closuers with event listeners 
+
+function attachEventListeners() {
+    let count = 0;
+    const button = document.getElementById("butt");
+    if(button) {
+        button.addEventListener("click", function xyz(){
+            console.log("Button clicked",++count);
+        });
+    } else {
+        console.log("Button element not found");
+    }
+}
+
+attachEventListeners();
+
+
+
+//Anonymous function i.e function without the name
+/*function(){
+
+}
+//function statement or function declaration
+function a(){
+    console.log("a called")
+}
+
+
+//First class function
+param1 is parameters and argu is argument
+//function expression
+const b=function(params1)
+{gument
+    return function xyz(){
+       
+     }
+}
+
+b(argu);
+
+*/
+
+//Callbacks and event stener
+
+//the callback function is the function we oassed to the other functuon as argument
+
